d009faa09bebe96c769e92f81ce9b19ca344b8c2
Eduardo <eduardo.m.silva53@aluno.senai.br>
2026-03-13 11:24:11 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GestaoEventos.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoEventos.Data;
+    public  class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria>  Categorias { get; set; }
+
+    public DbSet<Local> Locais { get; set; }
+
+
+
+
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoEventos.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "O nome da categoria é obrigatório!")]
+
+        public string Nome { get; set; } = string.Empty;
+    }
+}
